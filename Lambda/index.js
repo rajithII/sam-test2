8a62f975a0a63275730eba2817d5a07e7c869803
rajithII <30283449+rajithII@users.noreply.github.com>
2018-04-12 11:31:34 +0530

    Update index.js

--- Lambda/index.js
+++ Lambda/index.js
@@ -1,4 +1,4 @@
 exports.GreetingLambda = (event, context, callback) => {
 	    // Succeed with the string "Hello world!"
-	     callback(null, 'Test Lambda for dev and staging!!');
+	     callback(null, 'Test Lambda for production build!!');
 };
