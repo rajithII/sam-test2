f6e7af65e20d23fe33cf4b09ee292abe518f1caa
rajithII <30283449+rajithII@users.noreply.github.com>
2018-04-12 07:53:03 +0530

    Update index.js

--- Lambda/index.js
+++ Lambda/index.js
@@ -1,4 +1,4 @@
 exports.GreetingLambda = (event, context, callback) => {
 	    // Succeed with the string "Hello world!"
-	     callback(null, 'Hello World LAMBDA!!');
+	     callback(null, 'HELLO WORLD LAMBDA!!');
 };
