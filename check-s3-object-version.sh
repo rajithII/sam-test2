a74c262d8d8eb5d2ffc6d505deb108a3110c24a4
Rajith <rajith.v@iinerds.com>
2018-04-13 13:43:53 +0530

    parameterized

--- check-s3-object-version.sh
+++ check-s3-object-version.sh
@@ -5,6 +5,7 @@
 #version         :0.1    
 #usage		 :This script will retrieve the version id of latest lambda zip file from s3 bucket.
 #====================================================================================================
+sed -i -e 's/object_name/'"$1"'/g' data.yaml
 version_id=$(aws s3api get-object --bucket aadhri-test-buck --key $1 outfile | grep "VersionId" | awk '{ print $2 }'| tr -d '",')
 if [ $? != 0 ]; then
 	   echo "Version does not exist"
