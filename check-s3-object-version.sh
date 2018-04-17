46c3e17b27562397f49b334254481c51e6e50fca
Rajith <rajith.v@iinerds.com>
2018-04-17 18:21:49 +0530

    buildspec modified

--- check-s3-object-version.sh
+++ check-s3-object-version.sh
@@ -24,34 +24,34 @@ aws s3 cp $Lambda_zip s3://$S3_bucket_name/
 cd ..
 Current_lambda_version="\$LATEST"
 
-#Replace the Lambda template values to the original values in data.yaml
-sed -i -e 's/Object-name/'"$Lambda_zip"'/g' data.yaml
-sed -i -e 's/Bucket-name/'"$S3_bucket_name"'/g' data.yaml
-sed -i -e 's/Lambda-function-name/'"$Function_name"'/g' data.yaml
-sed -i -e 's/Function-description/'"$Function_description"'/g' data.yaml
-sed -i -e 's/Runtime-env/'"$Runtime_env"'/g' data.yaml
+#Replace the Lambda template values to the original values in inputTemplate.yaml
+sed -i -e 's/Object-name/'"$Lambda_zip"'/g' inputTemplate.yaml
+sed -i -e 's/Bucket-name/'"$S3_bucket_name"'/g' inputTemplate.yaml
+sed -i -e 's/Lambda-function-name/'"$Function_name"'/g' inputTemplate.yaml
+sed -i -e 's/Function-description/'"$Function_description"'/g' inputTemplate.yaml
+sed -i -e 's/Runtime-env/'"$Runtime_env"'/g' inputTemplate.yaml
 
-#Replace the API template values to the original values in data.yaml
-sed -i -e 's/APIName/'"$API_Name"'/g' data.yaml
-sed -i -e 's/API-Description/'"$API_Description"'/g' data.yaml
-sed -i -e 's/Path-Part/'"$Path_Part"'/g' data.yaml
+#Replace the API template values to the original values in inputTemplate.yaml
+sed -i -e 's/APIName/'"$API_Name"'/g' inputTemplate.yaml
+sed -i -e 's/API-Description/'"$API_Description"'/g' inputTemplate.yaml
+sed -i -e 's/Path-Part/'"$Path_Part"'/g' inputTemplate.yaml
 
 #Substitute the value of S3ObjectVersion with the current version id of zip file which is uploaded to s3 bucket.
 Version_id=$(aws s3api get-object --bucket aadhri-test-buck --key $Lambda_zip outfile | grep "VersionId" | awk '{ print $2 }'| tr -d '",')
 if [ $? != 0 ]; then
 	   echo "Version does not exist"
 else
-	   sed -i -e 's/Obj-version-id/'"$Version_id"'/' data.yaml  
+	   sed -i -e 's/Obj-version-id/'"$Version_id"'/' inputTemplate.yaml  
 fi
 
 #Pointing prod alias with the current version of lambda function. This step is necessary to keep the production always points to the current version of lambda during the update stack process. 
 Lambda_version=$(aws lambda get-alias --function-name $Function_name --name PROD | grep "FunctionVersion" | awk '{print $2}' | tr -d '",')
 
 if [ $? != 0 ]; then
-	sed 's/$LATEST/'"$Current_lambda_version"'/g' data.yaml
+	sed 's/$LATEST/'"$Current_lambda_version"'/g' inputTemplate.yaml
 else
-	sed ':a;N;$!ba;s/\$LATEST/'"$Lambda_version"'/3' data.yaml
+	sed ':a;N;$!ba;s/\$LATEST/'"$Lambda_version"'/3' inputTemplate.yaml
 fi
 
 #Packaging the cloudformation template
-aws cloudformation package --template-file data.yaml --s3-bucket $S3_bucket_name --output-template-file outputdata.yaml
+aws cloudformation package --template-file inputTemplate.yaml --s3-bucket $S3_bucket_name --output-template-file outputTemplate.yaml
