a1f33067fdf42af748bfa67a7eeb28b5973abbc0
rajithII <30283449+rajithII@users.noreply.github.com>
2018-04-26 15:10:57 +0530

    Update productionbuild.sh

--- productionbuild.sh
+++ productionbuild.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo apt-get update -y
 source lambda-variables.txt
 aws lambda publish-version --function-name $Function_name > lambda-version.txt
 New_version=$(cat lambda-version.txt | grep "Version" | awk '{ print $2 }' | tr -d '",')
